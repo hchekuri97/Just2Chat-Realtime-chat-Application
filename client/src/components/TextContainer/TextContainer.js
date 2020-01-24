39f79d542149c88386462b7c2145a2ed36427a9c
Harshini Chekuri <Harshini Chekuri>
2020-01-24 06:41:02 -0500

    first commit

---
+++ client/src/components/TextContainer/TextContainer.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import onlineIcon from '../../icons/onlineIcon.png';
+
+import './TextContainer.css';
+
+const TextContainer = ({ users }) => (
+  <div className="textContainer">
+    <div>
+      <h1>Welcome to Just2Chat <span role="img" aria-label="emoji">💬</span></h1>
+      <h2>Connecting a billion of people at one place </h2>
+      <h2>Get Involved<span role="img" aria-label="emoji">⬅️</span></h2>
+    </div>
+    {
+      users
+        ? (
+          <div>
+            <h1>Active People:</h1>
+            <div className="activeContainer">
+              <h2>
+                {users.map(({name}) => (
+                  <div key={name} className="activeItem">
+                    {name}
+                    <img alt="Online Icon" src={onlineIcon}/>
+                  </div>
+                ))}
+              </h2>
+            </div>
+          </div>
+        )
+        : null
+    }
+  </div>
+);
+
+export default TextContainer;
